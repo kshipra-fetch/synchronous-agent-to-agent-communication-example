deb890216b050300f14e89cad860cef46689c5ee
kshipra-fetch <kshipra.dhame@fetch.ai>
2025-03-11 14:07:48 +0000

    Update agent1.py

--- agent1.py
+++ agent1.py
@@ -5,7 +5,7 @@ class Message(Model):
     message: str
 
 class Response(Model):
-    number: int
+    response: str
 
 agent = Agent(name="alice", port=5001, endpoint = 'http://localhost:5001/submit')
 
@@ -14,9 +14,9 @@ agent2_address = ""       #run agent2.py, copy the agent's address and paste her
 @agent.on_interval(period=30.0)
 async def send_message(ctx: Context):
     msg = Message(message="hello from alice!")
-    reply, status = await ctx.send_and_receive(agent2_address, msg, response_type=Message)
-    if isinstance(reply, Message):
-        ctx.logger.info(f"Received awaited response from bob: {reply.message}")
+    reply, status = await ctx.send_and_receive(agent2_address, msg, response_type=Response)
+    if isinstance(reply, Response):
+        ctx.logger.info(f"Received awaited response from bob: {reply.response}")
     else:
         ctx.logger.info(f"Failed to receive response from bob: {status}")
 
